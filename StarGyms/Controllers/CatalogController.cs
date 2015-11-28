40b8de7d193c32e0f88b91059f5ad55d05133821
arisahiramosTFS <arisahi_ramos@toyota.com>
2015-11-28 10:46:20 -0600

    Se Recuperan Codigo de Controladodes

--- StarGyms/Controllers/CatalogController.cs
+++ StarGyms/Controllers/CatalogController.cs
@@ -3,19 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using StarGyms.Models;
+using System.ComponentModel.DataAnnotations;
 
-namespace StarGyms.Controllers
+namespace StarGyms.Models
 {
-    public class CatalogController : Controller
+    public class CustomCatalogModel
     {
-        // GET: Catalog
-        public ActionResult CatGeneral()
+        [Display(Name = "Catalogo")]
+        public int IdCatalogo { get; set; }
+        [Display(Name = "Elemento")]
+        public int Id { get; set; }
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+        [Display(Name = "Seleccionado")]
+        public bool Default1 { get; set; }
+        [Display(Name = "Activo")]
+        public bool Activo { get; set; }
+    }
+
+    public class CatalogModel
+    {
+        public int IdCatalogo { get; set; }
+        public IEnumerable<CustomCatalogModel> CatalogosTitulo { get; set; }
+        public IEnumerable<CustomCatalogModel> Catalogos { get; set; }
+
+        public static IEnumerable<CustomCatalogModel> CatGeneral(int IdCatalog)
         {
-            CatalogModel m = new CatalogModel();
-            m.CatalogosTitulo = CatalogModel.CatGeneralTitle(false);
-            m.Catalogos = CatalogModel.CatGeneral(0);
-            return View(m);
+            Entities.StartGymDB db = new Entities.StartGymDB();
+            if (IdCatalog == 0)
+            {
+                return db.CatalogoGens.OrderBy(m => m.IdCatalogo).OrderBy(m => m.Id).Select(m => new CustomCatalogModel { IdCatalogo = m.IdCatalogo, Id = m.Id, Descripcion = m.Descripcion, Activo = m.Activo, Default1 = m.Default1 }).AsEnumerable();
+            }
+            else
+            {
+                return db.CatalogoGens.Where(m => m.IdCatalogo == IdCatalog && m.Activo == true).OrderBy(m => m.Descripcion).Select(m => new CustomCatalogModel { IdCatalogo = m.IdCatalogo, Id = m.Id, Descripcion = m.Descripcion, Activo = m.Activo, Default1 = m.Default1 }).AsEnumerable();
+            }
         }
+
+        public static IEnumerable<CustomCatalogModel> CatGeneralTitle(bool OnlyActive)
+        {
+            Entities.StartGymDB db = new Entities.StartGymDB();
+            if (OnlyActive)
+            {
+                return db.CatalogoGens.Where(m => m.Id == 0 && m.Activo == true).OrderBy(m => m.Descripcion).Select(m => new CustomCatalogModel { IdCatalogo = m.IdCatalogo, Id = m.Id, Descripcion = m.Descripcion, Activo = m.Activo, Default1 = m.Default1 }).AsEnumerable();
+            }
+            else
+            {
+                return db.CatalogoGens.Where(m => m.Id == 0).OrderBy(m => m.Descripcion).Select(m => new CustomCatalogModel { IdCatalogo = m.IdCatalogo, Id = m.Id, Descripcion = m.Descripcion, Activo = m.Activo, Default1 = m.Default1 }).AsEnumerable();
+            }
+
+        }
+
+        public static IEnumerable<Entities.Sucursale> Sucursales()
+        {
+            Entities.StartGymDB db = new Entities.StartGymDB();
+            return db.Sucursales.OrderBy(m => m.Nombre).AsEnumerable();
+        }
+
     }
 }
--- StarGyms/Controllers/JsonController.cs
+++ StarGyms/Controllers/JsonController.cs
@@ -7,12 +7,63 @@ using StarGyms.Models;
 
 namespace StarGyms.Controllers
 {
-    public class JsonController : Controller
+    public class PeopleController : Controller
     {
-        public JsonResult ClienteAutoComplete(string buscar, int? items)
+        // GET: People
+        public ActionResult Index()
         {
-            JsonModel js = new JsonModel();
-            return Json(js.jBuscarCliente(buscar, items), JsonRequestBehavior.AllowGet);
+            SearchModel m = new SearchModel
+            {
+                Action = "Search",
+                Controller = "People",
+                Model = PeopleModel.GetCustomSearchPeople(string.Empty, 20),
+                Persona = new CustomPeopleModel()
+            };
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult Search(SearchModel m)
+        {
+            IEnumerable<CustomPeopleModel> r = PeopleModel.GetCustomSearchPeople(m.Buscar.Trim());
+            if (r != null && r.Count() == 1)
+            {
+                return PartialView("_PeopleEdit", new PeopleModel{ IdPersona = r.First().IdPersona, Persona = r.First()});
+            }
+            else if (r != null && r.Count() > 1)
+            {
+                return PartialView("_PeopleList", r);
+            }
+            return PartialView("_PeopleList", new List<CustomPeopleModel>());
+        }
+
+        public ActionResult Add()
+        {
+            return View( new PeopleModel());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            PeopleModel m = new PeopleModel{ IdPersona = id };
+            if (id != 0)
+            {
+                m.GetCustomSinglePeople();
+            }
+            return PartialView("_PeopleEdit",m);
+        }
+
+        [HttpPost]
+        public ActionResult Save(CustomPeopleModel m)
+        {
+            if(ModelState.IsValid)
+            {
+                PeopleModel.SaveNew(m);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("_PeopleEdit", m);
+            }
         }
     }
 }
